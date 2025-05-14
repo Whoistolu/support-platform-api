85207a6de7c64407442525145b55d2cca6531ca8
Whoistolu <digittolu25@gmail.com>
2025-05-14 11:12:23 +0100

    Define permissions needed for this app

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,29 +4,18 @@ class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the user here. For example:
-    #
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, published: true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+    return unless user
+
+    if user.agent?
+      can :manage, SupportTicket
+      can :manage, Comment
+    elsif user.customer?
+      can :create, SupportTicket
+      can :read, SupportTicket, user_id: user.id
+      can :create, Comment do |comment|
+        comment.support_ticket.agent_has_commented?
+      end
+      can :read, Comment, support_ticket: { user_id: user.id }
+    end
   end
 end
