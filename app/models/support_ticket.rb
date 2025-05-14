a4145256c92477688e9af851de1c18f8901d6281
Whoistolu <digittolu25@gmail.com>
2025-05-14 09:39:25 +0100

    Add attachments and comments association to SupportTicket

--- app/models/support_ticket.rb
+++ app/models/support_ticket.rb
@@ -2,4 +2,6 @@ class SupportTicket < ApplicationRecord
   belongs_to :user
 
   enum status: { open: 0, pending: 1, closed: 2 }
+  has_many_attached :attachments
+  has_many :comments, dependent: :destroy
 end
