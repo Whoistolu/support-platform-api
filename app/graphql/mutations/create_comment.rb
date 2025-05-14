c2cf687bbdf0f342adff78fae5963ce34d151af6
Whoistolu <digittolu25@gmail.com>
2025-05-14 10:50:45 +0100

    Add CreateComment GraphQL mutation with support_ticket association and body input

---
+++ app/graphql/mutations/create_comment.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+module Mutations
+  class CreateComment < BaseMutation
+    argument :support_ticket_id, ID, required: true
+    argument :body, String, required: true
+
+    field :comment, Types::CommentType, null: true
+    field :errors, [String], null: false
+
+    def resolve(support_ticket_id:, body:)
+      user = context[:current_user]
+      return { comment: nil, errors: ["Authentication required"] } unless user
+
+      ticket = SupportTicket.find_by(id: support_ticket_id)
+      return { comment: nil, errors: ["Support ticket not found"] } unless ticket
+
+      comment = ticket.comments.build(user: user, body: body)
+
+      if comment.save
+        { comment: comment, errors: [] }
+      else
+        { comment: nil, errors: comment.errors.full_messages }
+      end
+    end
+  end
+end
