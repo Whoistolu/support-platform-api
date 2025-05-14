32ae3c0bd4192064bdec7ced93e7ac8e5ba1c4ce
Whoistolu <digittolu25@gmail.com>
2025-05-14 11:04:28 +0100

    Add attachment validation to SupportTicket for file type and size using ActiveStorageValidations

--- app/models/support_ticket.rb
+++ app/models/support_ticket.rb
@@ -5,6 +5,12 @@ class SupportTicket < ApplicationRecord
   has_many_attached :attachments
   has_many :comments, dependent: :destroy
 
+  validates :attachments,
+  blob: {
+    content_type: ['image/png', 'image/jpg', 'image/jpeg', 'application/pdf'],
+    size_range: 1..10.megabytes
+  }
+
   def agent_has_commented?
     comments.joins(:user).where(users: { role: "agent" }).exists?
   end
