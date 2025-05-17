f8bbe814066e17c999676f9e9a20d915788f7e91
Whoistolu <digittolu25@gmail.com>
2025-05-17 13:58:16 +0100

    Downgrade migration version from 8.0 to 7.1 for Rails compatibility

--- db/migrate/20250514082044_create_support_tickets.rb
+++ db/migrate/20250514082044_create_support_tickets.rb
@@ -1,4 +1,4 @@
-class CreateSupportTickets < ActiveRecord::Migration[8.0]
+class CreateSupportTickets < ActiveRecord::Migration[7.1]
   def change
     create_table :support_tickets do |t|
       t.string :title
