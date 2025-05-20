1a7ec994ff201ce97ece2479837e53897d4cdce6
Whoistolu <digittolu25@gmail.com>
2025-05-20 14:48:30 +0100

    Set up API routes for custom Devise controllers and session handling

--- config/application.rb
+++ config/application.rb
@@ -5,6 +5,8 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
+Dotenv::Railtie.load if defined?(Dotenv)
+
 
 module SupportPlatformApi
   class Application < Rails::Application
@@ -25,17 +27,10 @@ module SupportPlatformApi
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
 
-    # Configuration for the application, engines, and railties goes here.
-    #
-    # These settings can be overridden in specific environments using the files
-    # in config/environments, which are processed later.
-    #
-    # config.time_zone = "Central Time (US & Canada)"
-    # config.eager_load_paths << Rails.root.join("extras")
 
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
+    config.middleware.use ActionDispatch::Cookies
+    config.middleware.use ActionDispatch::Session::CookieStore
+
     config.api_only = true
   end
 end
